f41f6f0fd44d269710e1dd514177c53df615664d
ooriz <iiroz@yandex.ru>
2024-10-22 11:09:43 +0300

    Create test.php
    first

---
+++ test.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+
+
+/////////2
+
+
+
+
+
+
+
+
+
+
+
+
+$atackR=0;
+if($atackR!='1'){echo '!';}
+die;
+
+
+
+
+
+$prefix='1';
+$hh='';
+$l=0;$s=strrchr($hh,$prefix);if($s!==false){$l=strlen(substr($s,1));}
+echo $l.'';die;
+
+
+
+
+
+?>
